38ecce92479ff1f92128cb0d451187347802e00d
JohnC <chewyunpin@gmail.com>
2026-05-13 16:11:29 +0800

    all of setonix/ folder

---
+++ setonix/mv_to_DB_volume.py
@@ -0,0 +1,3 @@
+# See: https://docs.databricks.com/aws/en/dev-tools/sdk-python#manage-files-in-unity-catalog-volumes
+# https://docs.databricks.com/aws/en/volumes/volume-files#manage-files-in-volumes-with-the-rest-api
+from databricks.sdk import WorkSpaceClient
