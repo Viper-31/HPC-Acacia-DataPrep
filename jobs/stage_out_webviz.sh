7fb379b4941b8e07b274a26abd18d877371515a6
JohnC <chewyunpin@gmail.com>
2026-05-25 14:13:48 +0800

    Added space for readability

--- jobs/stage_out_webviz.sh
+++ jobs/stage_out_webviz.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 salloc -p copy --nodes=1 --ntasks=3 --cpus-per-task=8 --mem=20G --time=02:00:00 --job-name=data_out_webviz
+
 module load rclone/1.68.1
 
 rclone copy $MYSCRATCH/vz_kerchunk jchew:webviz \
