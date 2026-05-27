1b889a6aaf30fb592dc3ff8157054be1e90836da
JohnC <chewyunpin@gmail.com>
2026-05-27 10:27:47 +0800

    Sucess: Stage to Acacia webviz bucket

--- jobs/stage_out_webviz.sh
+++ jobs/stage_out_webviz.sh
@@ -1,12 +1,22 @@
 # !/bin/bash
-salloc -p copy --nodes=1 --ntasks=3 --cpus-per-task=8 --mem=20G --time=02:00:00 --job-name=data_out_webviz
+salloc -p copy --nodes=1 --ntasks=3 --cpus-per-task=8 --mem=40G --time=02:00:00 --job-name=data_out_webviz
 
 module load rclone/1.68.1
 
-rclone copy $MYSCRATCH/vz_kerchunk jchew:webviz \
+# dry-run first
+rclone copy $MYSCRATCH/kerchunk_webviz jchew:webviz --include "/DPIRD/**" --include "/ECMWF/2024/*" --dry-run -v
+
+# full transfer
+
+rclone copy $MYSCRATCH/kerchunk_webviz jchew:webviz \
+         --include "/DPIRD/**" \
+         --include "/ECMWF/2024/**" \
          --progress \
          --transfers 24 \
-         --checkers 24
+         --checkers 24 \
+         -v
 
 # Verify transfer success
 rclone ls jchew:webviz | head -n 20
+
+exit
